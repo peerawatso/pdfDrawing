6add63956c6cc2b010a44649fcf29f6e38c99763
DSolution Macbook <thetong1911.2@gmail.com>
2019-12-29 11:14:21 +0700

    test

--- pdfDrawing/Drawing/PDFText.swift
+++ pdfDrawing/Drawing/PDFText.swift
@@ -100,7 +100,7 @@ extension PDFText: DrawingGestureTextDelegate {
 //        textFieldMultiline21.isMultiline = true
 //        page.addAnnotation(textFieldMultiline21)
         
-        let annotation = DrawingTextAnnotation(bounds: CGRect(x: 100, y: 100, width: 100, height: 20), forType: .freeText, withProperties: nil)
+        let annotation = DrawingTextAnnotation(bounds: page.bounds(for: pdfView.displayBox), forType: .freeText, withProperties: nil)
          annotation.contents = "Hello, world!"
          annotation.font = UIFont.systemFont(ofSize: 15.0)
          annotation.fontColor = .blue
